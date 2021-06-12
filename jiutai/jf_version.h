7eb29d857a6f2fd4a8f33f74e0b8070eea4b13e4
minz <ned0000@sina.com>
2021-06-12 09:24:29 +0800

    add version header file.

---
+++ jiutai/jf_version.h
@@ -0,0 +1,43 @@
+/**
+ *  @file jf_version.h
+ *
+ *  @brief Header file defines the version information of Jiufeng.
+ *
+ *  @author Min Zhang
+ *
+ *  @note
+ *  -# The version has format with "major.minor.build".
+ */
+
+#ifndef JIUFENG_VERSION_H
+#define JIUFENG_VERSION_H
+
+/* --- standard C lib header files -------------------------------------------------------------- */
+
+
+/* --- internal header files -------------------------------------------------------------------- */
+
+
+/* --- constant definitions --------------------------------------------------------------------- */
+
+/** Define the major version.
+ */
+#define JF_VERSION_MAJOR                       (1)
+
+/** Define the major version.
+ */
+#define JF_VERSION_MINOR                       (0)
+
+/** Define the build number.
+ */
+#define JF_VERSION_BUILD                       (1)
+
+/* --- data structures -------------------------------------------------------------------------- */
+
+
+/* --- functional routines ---------------------------------------------------------------------- */
+
+
+#endif /*JIUFENG_VERSION_H*/
+
+/*------------------------------------------------------------------------------------------------*/
